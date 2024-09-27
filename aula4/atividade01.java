5cbb0427962cef770da4ef7b5685cbb8cb2fa427
⁦Noir炎⁩ <165437398+3OOOO@users.noreply.github.com>
2024-09-27 14:48:21 +0100

    Add files via upload

---
+++ aula4/atividade01.java
@@ -0,0 +1,10 @@
+public class atividade01 {
+    public static void main(String[] args) {
+        System.out.println("Os convidados da festa são:");
+        String [] convidados = {"Ana","Olivia","Oscar","Maria","Francisco","Pedro","Miguel"};
+
+        for (String convidado : convidados) {
+            System.out.println(convidado);
+        }
+    }
+}
