aaa2792a07badcb6ec373a927526069b72c1f1a5
⁦Noir炎⁩ <165437398+3OOOO@users.noreply.github.com>
2024-10-11 15:34:03 +0100

    Add files via upload

---
+++ atividade21.java
@@ -0,0 +1,16 @@
+public class exercicios1 {
+    public static void main(String[] args) {
+        System.out.println("Crie uma matriz 2x3 que gere 3x2");
+        int matriz[][] = {{1, 2, 3},{4, 5, 6}}; // Criação da matriz
+        
+        // Corre a Linhas inteira
+        for (int i = 0; i < matriz.length; i++) { 
+            // Corre a Colunas inteira
+            for (int l = 0; l < matriz[i].length; l++) {
+                System.out.print(matriz[i][l] + " ");
+            }
+            System.out.println(" ");
+        }
+        
+    }
+}
