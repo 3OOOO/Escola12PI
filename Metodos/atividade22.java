da5b11a36efbefa935a877728f5c8b8169cf51ad
⁦Noir炎⁩ <165437398+3OOOO@users.noreply.github.com>
2024-10-11 16:18:31 +0100

    Add files via upload

---
+++ Metodos/atividade22.java
@@ -0,0 +1,19 @@
+package Metodos;
+
+
+public class atividade22 {
+    public static void main(String[] args) {
+       nex();
+    }
+
+    public static void nex() {
+        System.out.println("Seja Bem Vindo!");
+
+        System.out.println("== Menu ==");
+        System.out.println("1 - Entradas");
+        System.out.println("2 - Prato Carne");
+        System.out.println("3 - Prato Peixe");
+        System.out.println("4 - Sobremesa");
+        System.out.println("5 - Sair");
+    }
+}
